730dce45874db0c05f74445aa16e45a5c1a27a22
Syed Farhan <syedfarhandev@gmail.com>
2021-12-23 01:25:42 -0500

    Install Ant Design and add coniguration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Button type="primary">Button</Button>
         </a>
       </header>
     </div>
